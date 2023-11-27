2c4769e52aec8ee248cde9567b987a25811c0dbc
seanfitz922 <seanfitz922@gmail.com>
2023-11-26 21:56:51 -0600

    presentation features

--- bezier.py
+++ bezier.py
@@ -28,7 +28,9 @@ font = pygame.font.Font(None, FONT_SIZE)
 # List to store control points
 control_points = []
 
-# formula from https://stackoverflow.com/questions/5634460/quadratic-b%C3%A9zier-curve-calculate-points
+# formula from https://en.wikipedia.org/wiki/B%C3%A9zier_curve and 
+# https://math.stackexchange.com/questions/26846/is-there-an-explicit-form-for-cubic-b%C3%A9zier-curves
+
 def cubic_bezier(t, control_points):
     P0, P1, P2, P3 = control_points
     x = (1 - t) ** 3 * P0[0] + 3 * (1 - t) ** 2 * t * P1[0] + 3 * (1 - t) * t**2 * P2[0] + t**3 * P3[0]
@@ -39,8 +41,7 @@ def generate_polynomial_equation(control_points):
     P0, P1, P2, P3 = control_points
     x_term = f"(1 - t)^3 * {P0[0]} + 3 * (1 - t)^2 * t * {P1[0]} + 3 * (1 - t) * t^2 * {P2[0]} + t^3 * {P3[0]}"
     y_term = f"(1 - t)^3 * {P0[1]} + 3 * (1 - t)^2 * t * {P1[1]} + 3 * (1 - t) * t^2 * {P2[1]} + t^3 * {P3[1]}"
-    # return parametric equation
-    return f"B(t) = {x_term} + {y_term}"
+    return x_term, y_term
 
 def display_polynomial_text(polynomial, position):
     text = font.render("Polynomial: " + polynomial, True, TEXT_COLOR)
@@ -54,6 +55,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # mouse position from https://stackoverflow.com/questions/66349281/pygame-function-to-draw-a-line-between-two-points-is-not-working
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Left mouse button
             if event.button == 1 and drawing:  
@@ -93,12 +95,17 @@ while running:
         x_values = [cubic_bezier(t, control_points)[0] for t in t_values]
         y_values = [cubic_bezier(t, control_points)[1] for t in t_values]
         # Curve drawing
+        # Connect points with lines from https://lorenzod8n.wordpress.com/2007/05/27/pygame-tutorial-2-drawing-lines/
         pygame.draw.lines(screen, CURVE_COLOR, False, list(zip(x_values, y_values)), 5)
         
         # Calculate and display the polynomial equation
         polynomial = generate_polynomial_equation(control_points)
-        print(polynomial)
-        display_polynomial_text(polynomial, (10, 10))
+
+        # print(f"x(t) = {polynomial[0]}")
+        # print(f"y(t) = {polynomial[1]}")
+
+        display_polynomial_text("x(t)= " + polynomial[0], (10, 10))
+        display_polynomial_text("y(t)= " + polynomial[1], (10, 50))
 
     pygame.display.flip()
 
